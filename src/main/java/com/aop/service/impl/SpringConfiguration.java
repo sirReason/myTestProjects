ee91258cc8096c4b4e76b63d340eee23f4be1249
liyou <liyou@iquantex.com>
2022-04-15 16:27:41 +0800

    impl删除

--- src/main/java/com/aop/service/impl/SpringConfiguration.java
+++ src/main/java/com/aop/service/impl/SpringConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@ComponentScan("com.aop.service.impl")
+@ComponentScan("com.aop.service")
 @EnableAspectJAutoProxy(exposeProxy = true)//开启spring注解aop配置的支持
 public class SpringConfiguration {
 }
